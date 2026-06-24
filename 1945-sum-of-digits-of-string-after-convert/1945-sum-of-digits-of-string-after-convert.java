ea84742133c2a62448b31badb9bcc0962a2eaab4
Dhasharath04 <dhasharathjatoth@gmail.com>
2026-06-24 22:08:56 +0530

    Time: 1 ms (88.08%), Space: 43.2 MB (48.66%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        int n=s.length();
+         int s1=0;
+        for(int i=0;i<n;i++)
+        {
+            int d=s.charAt(i)-'a'+1;
+        
+       
+        while(d>0)
+        {
+            s1+=d%10;
+            d=d/10;
+        }
+        }
+        for(int i=1;i<k;i++)
+        {
+            int ns=0;
+            while(s1>0)
+            {
+                ns+=s1%10;
+                s1=s1/10;
+            }
+            s1=ns;
+        }
+
+        return s1;
+    }
+}
